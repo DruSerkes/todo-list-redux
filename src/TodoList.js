15cda27c82346801f123442e9b0057c98c4b812a
Dru Serkes <DruSerkes@gmail.com>
2020-08-10 21:45:02 -0400

    refactor for redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Todo from './Todo';
 import NewTodoForm from './NewTodoForm';
-import EditTodoForm from './EditTodoForm';
+// import EditTodoForm from './EditTodoForm';
 import { v4 as uuid } from 'uuid';
+import { useSelector, useDispatch } from 'react-redux';
 
 /**
  * TodoList - this component renders the NewTodoForm component 
@@ -10,71 +11,77 @@ import { v4 as uuid } from 'uuid';
  */
 
 const TodoList = () => {
-	const INITIAL_STATE = JSON.parse(localStorage.getItem('todos')) || [];
-	const [ todos, setTodos ] = useState(INITIAL_STATE);
+	// const INITIAL_STATE = JSON.parse(localStorage.getItem('todos')) || [];
+	const todos = useSelector((state) => state.todos);
+	const dispatch = useDispatch();
 
 	const addTodo = ({ text }) => {
 		if (!text) return;
-		const newTodo = { text, id: uuid(), editing: false, complete: false };
-		const updatedTodos = [ ...todos, newTodo ];
-		setTodos((todos) => updatedTodos);
-		updateLocalStorage(updatedTodos);
+		const newTodo = { text, id: uuid() };
+		// const updatedTodos = [ ...todos, newTodo ];
+		// setTodos((todos) => updatedTodos);
+		dispatch({ type: 'ADD_TODO', payload: newTodo });
+		// updateLocalStorage(updatedTodos);
 	};
 
-	const updateLocalStorage = (updatedTodos) => {
-		localStorage.setItem('todos', JSON.stringify(updatedTodos));
-	};
+	// const updateLocalStorage = (updatedTodos) => {
+	// 	localStorage.setItem('todos', JSON.stringify(updatedTodos));
+	// };
 
 	const removeTodo = (id) => {
-		setTodos((todos) => todos.filter((todo) => todo.id !== id));
-		const storedTodos = JSON.parse(localStorage.getItem('todos'));
-		const updatedTodos = storedTodos.filter((todo) => todo.id !== id);
-		updateLocalStorage(updatedTodos);
+		// setTodos((todos) => todos.filter((todo) => todo.id !== id));
+		dispatch({ type: 'REMOVE_TODO', id });
+		// const storedTodos = JSON.parse(localStorage.getItem('todos'));
+		// const updatedTodos = storedTodos.filter((todo) => todo.id !== id);
+		// updateLocalStorage(updatedTodos);
 	};
 
-	const toggleComplete = (id) => {
-		const todo = todos.find((todo) => todo.id === id);
-		todo.complete = !todo.complete;
-		const updatedTodos = [ ...todos ];
-		setTodos((todos) => updatedTodos);
-		updateLocalStorage(updatedTodos);
-	};
+	// const toggleComplete = (id) => {
+	// 	const todo = todos.find((todo) => todo.id === id);
+	// 	todo.complete = !todo.complete;
+	// 	const updatedTodos = [ ...todos ];
+	// 	setTodos((todos) => updatedTodos);
+	// 	updateLocalStorage(updatedTodos);
+	// };
 
-	const toggleEditing = (id) => {
-		const todo = todos.find((todo) => todo.id === id);
-		todo.editing = !todo.editing;
-		setTodos((todos) => [ ...todos ]);
-	};
+	// const toggleEditing = (id) => {
+	// 	const todo = todos.find((todo) => todo.id === id);
+	// 	todo.editing = !todo.editing;
+	// 	setTodos((todos) => [ ...todos ]);
+	// };
 
-	const editTodo = ({ id, text }) => {
-		if (!text) return;
-		const todo = todos.find((todo) => todo.id === id);
-		todo.editing = !todo.editing;
-		todo.text = text;
-		setTodos((todos) => [ ...todos ]);
-	};
+	// const editTodo = ({ id, text }) => {
+	// 	if (!text) return;
+	// 	const todo = todos.find((todo) => todo.id === id);
+	// 	todo.editing = !todo.editing;
+	// 	todo.text = text;
+	// 	setTodos((todos) => [ ...todos ]);
+	// };
 
-	const checkIfEditing = (todo) => {
-		return todo.editing ? (
-			<EditTodoForm text={todo.text} id={todo.id} editTodo={editTodo} key={todo.id} />
-		) : (
-			<Todo
-				text={todo.text}
-				key={todo.id}
-				id={todo.id}
-				complete={todo.complete}
-				removeTodo={removeTodo}
-				toggleEditing={toggleEditing}
-				toggleComplete={toggleComplete}
-			/>
-		);
-	};
+	// const checkIfEditing = (todo) => {
+	// 	return todo.editing ? (
+	// 		<EditTodoForm text={todo.text} id={todo.id} editTodo={editTodo} key={todo.id} />
+	// 	) : (
+	// 		<Todo
+	// 			text={todo.text}
+	// 			key={todo.id}
+	// 			id={todo.id}
+	// 			complete={todo.complete}
+	// 			removeTodo={removeTodo}
+	// 			toggleEditing={toggleEditing}
+	// 			toggleComplete={toggleComplete}
+	// 		/>
+	// 	);
+	// };
 
 	return (
 		<div className="TodoList">
 			<h1 className="TodoList-Header">My Todo List</h1>
 			<NewTodoForm addTodo={addTodo} />
-			<div className="TodoList-Container">{todos.map((todo) => checkIfEditing(todo))}</div>
+			{/* <div className="TodoList-Container">{todos.map((todo) => checkIfEditing(todo))}</div> */}
+			<div className="TodoList-Container">
+				{todos.map((todo) => <Todo text={todo.text} key={todo.id} id={todo.id} removeTodo={removeTodo} />)}
+			</div>
 		</div>
 	);
 };
