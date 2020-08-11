3cb25475df36916ca3fd2d0ce8b0ec2acaa205f9
Dru Serkes <DruSerkes@gmail.com>
2020-08-11 01:01:01 -0400

    Updates

---
+++ precache-manifest.9f9225353b46d05397e977dfdeac1573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b72a1d2efeb4f6fcfedc43999519aa8",
+    "url": "/todo-list-redux/index.html"
+  },
+  {
+    "revision": "1114044049a1564e4f42",
+    "url": "/todo-list-redux/static/css/main.cb6f2749.chunk.css"
+  },
+  {
+    "revision": "2429713f46de0e95abe2",
+    "url": "/todo-list-redux/static/js/2.fe2b45ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list-redux/static/js/2.fe2b45ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1114044049a1564e4f42",
+    "url": "/todo-list-redux/static/js/main.93974b1e.chunk.js"
+  },
+  {
+    "revision": "06d861b11a9d018ba209",
+    "url": "/todo-list-redux/static/js/runtime-main.a0cf73d9.js"
+  }
+]);
